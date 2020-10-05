1960b48be077757f62fa7190e3537df930eeb02c
bartfaics <53113908+bartfaics@users.noreply.github.com>
2020-10-05 20:39:13 +0200

    Create fuggvenyekhz.py
    A harmadik óra feladatai

---
+++ fuggvenyekhz.py
@@ -0,0 +1,28 @@
+"""Feladatok függvények témakörben
+    Minden függyvénynél találsz egy meghatározást, hogy mit kell a függvénynek tennie.
+    Végezd el a feladatot! Ha valami nem egyértelmű, kérdezz! :)
+"""
+
+# Első feladat:
+# Készíts egy függvényt, ami meghatározza, hogy milyen rag kerüljön egy szó végére. 
+
+szavak = ["rózsa", "alma", "körte", "burgonya", "sütemény", "gyerek", "iskola", "tanár", "hal", "bor", "sárkány", "portálfegyver", "dimenziókapu"]
+ragok = ["nak", "nek"]
+
+# Tipp: az hogy milyen rag kerül a szó végére, az határozza meg hogy magas vagy mély hangrendű magánhangzók vannak benne
+# magas hangrendű: eéiíüűöő
+# mély hangrendű: aáoóuú
+# ha vegyesen van benne a két hangrend, akkor mély hangrendű lesz a rag
+
+# Második feladat:
+# Csinálj egy függvényt, ami meghatározza egy listában a legkisebb és legnagyobb elemet és ezeket kiírja
+
+myNumbers = [1, 5, 12, 0, -2, 91, -12, 30, 32]
+
+# Harmadik feladat:
+# Vedd az első függvényt, és fejleszd tovább úgy, hogy a magyar nyelvnek megfelelően ragozzon.
+# Ha a szó a rózsa, és a rag a nak, akkor azt írja ki, hogy rózsának.
+
+# +1 feladat:
+# készítsd el a harmadik feladat függvényét úgy, hogy a val és a vel ragok esetén is jól működjön
+# Például gyerek szó esetén gyerekkel legyen az eredmény.
