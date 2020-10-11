12aa0e43f52dcc5c8b512934d6a5ca95ba4565c5
bartfaics <53113908+bartfaics@users.noreply.github.com>
2020-10-11 15:24:22 +0200

    Class és objektum
    Ugye milyen klasszok a classok?

---
+++ classes.py
@@ -0,0 +1,12 @@
+# Első feladat
+# Készíts egy osztályt négylábú állatoknak legalább három tetszőleges tulajdonsággal és egy metódussal.
+
+# Második feladat 
+# Készíts az első feladatban készített osztálynak egy róka alosztályt. A konstruktor legyen ugyanaz, csak kapjon két extra metódust
+
+# Harmadik feladat
+# Készíts a második feladatban készített osztálynak egy újabb alosztályt, legyen a konstruktor is módosítva úgy hogy tartalmazza, az összes szülő osztály konstruktorainak elemeit.
+# Tartalmazzon két újabb metódust, az egyik egy getter a másik setter típusú legyen. Az osztály a vörös róka legyen.
+
+# Negyedik feladat
+# A vörös róka osztályból hozz létre egy objektumot. Az objektumot és/vagy az osztályt módosítsd úgy, hogy kiderüljön az ezeréves kérdésre a válasz: mit mond a róka?
